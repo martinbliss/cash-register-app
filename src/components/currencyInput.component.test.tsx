29919752f39764eaccbde757199c9ade73630b00
Martin Bliss <mbliss@anonymous.org>
2019-10-23 13:41:32 -0500

    Handicap

--- src/components/currencyInput.component.test.tsx
+++ src/components/currencyInput.component.test.tsx
@@ -8,31 +8,4 @@ describe('CurrencyInput', () => {
         render(<CurrencyInput value={23.99} ariaLabel="test" onChange={jest.fn()} />);
     });
 
-    it('should have an input when enabled', async () => {
-        const { baseElement } = render(<CurrencyInput ariaLabel="test" value={23.99} onChange={jest.fn()} />);
-        expect(baseElement.querySelector('input')).toBeTruthy();
-    });
-
-    it('should have a currency format', async () => {
-        const { baseElement, debug } = render(<CurrencyInput value={23.99} ariaLabel="test" onChange={jest.fn()} />);
-        const input = baseElement.querySelector('input');
-
-        if (input) {
-            expect(input.getAttribute('value')).toEqual('$23.99');
-        } else {
-            throw 'Input not found!';
-        }
-    });
-
-    it('should NOT have an input when disabled', async () => {
-        const { baseElement } = render(<CurrencyInput value={23.99} onChange={jest.fn()} disabled={true} />);
-        expect(baseElement.querySelector('input')).toBeFalsy();
-    });
-    it('should display a currency value when disabled', async () => {
-        const { findByText } = render(<CurrencyInput value={23.99} onChange={jest.fn()} disabled={true} />);
-        findByText('$23.99');
-    });
-    it('should not throw if aria label is missing when component is disabled', async () => {
-        render(<CurrencyInput value={23.22} disabled />);
-    });
 });
--- src/util/changeMaker.tsx
+++ src/util/changeMaker.tsx
@@ -37,25 +37,5 @@ export const makeChange = (total: number, tender: number): TransactionResult => 
 }
 
 const getDenominations = (balance: number): Denominations => {
-    const rankedDenominations = _.orderBy(Object.keys(denominationConfiguration).map(key => ({ key, ...(denominationConfiguration as DenominationConfiguration)[key] })), ['value'], ['desc']);
-
-    let remainingBalance = new Decimal(balance);
-    const result = rankedDenominations.reduce((sum, n) => {
-        if (remainingBalance.gt(0)) {
-            const splitRemainder = remainingBalance.dividedBy(n.value).floor();
-
-            if (splitRemainder.gte(1)) {
-                remainingBalance = remainingBalance.minus(new Decimal(n.value).times(splitRemainder));
-
-                sum[n.key] = {
-                    caption: n.caption,
-                    count: splitRemainder.toNumber()
-                };
-            }
-        }
-
-        return sum;
-    }, {} as Denominations);
-
-    return result;
+    return {};
 }
